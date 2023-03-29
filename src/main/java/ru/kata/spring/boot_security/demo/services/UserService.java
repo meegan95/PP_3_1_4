7f25888a72cd5f38fa903e54680530419475644f
meegan95 <122483262+meegan95@users.noreply.github.com>
2023-03-30 00:19:46 +0300

    clean 0.5 interfaces added

--- src/main/java/ru/kata/spring/boot_security/demo/services/UserService.java
+++ src/main/java/ru/kata/spring/boot_security/demo/services/UserService.java
@@ -1,56 +1,21 @@
 package ru.kata.spring.boot_security.demo.services;
 
 
-import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.stereotype.Service;
-import org.springframework.transaction.annotation.Transactional;
 import ru.kata.spring.boot_security.demo.models.User;
-import ru.kata.spring.boot_security.demo.repositories.UsersRepository;
-
 
 import java.util.List;
-import java.util.Optional;
 
-@Service
-@Transactional(readOnly = true)
-public class UserService {
+public interface UserService {
 
-    private final UsersRepository usersRepository;
+    List<User> findAll();
 
+    User findOne(int id);
 
-    @Autowired
-    public UserService(UsersRepository usersRepository) {
-        this.usersRepository = usersRepository;
-    }
+    User getPersonByUsername(String username);
 
-    public List<User> findAll() {
-        return usersRepository.findAll();
-    }
+    void save(User user);
 
-    public User findOne(int id) {
-        Optional<User> user = usersRepository.findById(id);
-        return user.orElse(null);
-    }
+    void update(int id, User updatedUser);
 
-    public User getPersonByUsername(String username) {
-        Optional<User> user = usersRepository.findByUsername(username);
-        return user.orElse(null);
-    }
-
-    @Transactional
-    public void save(User user) {
-        usersRepository.save(user);
-    }
-
-    @Transactional
-    public void update(int id, User updatedUser) {
-        updatedUser.setId(id);
-        usersRepository.save(updatedUser);
-    }
-
-    @Transactional
-    public void delete(int id) {
-        usersRepository.deleteById(id);
-    }
-
+    void delete(int id);
 }
